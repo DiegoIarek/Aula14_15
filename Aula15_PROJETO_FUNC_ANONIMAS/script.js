9e36ffe4e524f40a709173fd7f7682ec5ab7b7e1
DiegoIarek <137222245+DiegoIarek@users.noreply.github.com>
2024-10-07 10:42:40 -0300

    Add files via upload

---
+++ Aula15_PROJETO_FUNC_ANONIMAS/script.js
@@ -0,0 +1,18 @@
+function somar(a,b){
+    let total = a+b
+    console.log(total)
+}
+somar(2,10);
+
+//atribuir uma função anônima a uma variável
+let subtrair =(num1,num2)=>{
+    let total = num1-num2;
+    console.log(total);
+}
+
+subtrair(15,17);
+
+let numeros =[1,2,3,10]
+numero.map((item)=>{
+    console.log(item)
+})
